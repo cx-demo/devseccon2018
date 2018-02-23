1b74681428a1f5e18bdf8917affbcdbbeff00ddd
root <root@localhost.localdomain>
2018-02-23 12:35:49 +0800

    added mongo credentials

--- start_app_server.sh
+++ start_app_server.sh
@@ -4,7 +4,7 @@
 # READ SECRET!
 # export MONGODB_CREDENTIALS=mongodb://test:testtest@ds239638.mlab.com:39638/
 
-export mongodb://cxdemo:cxdemo@ds247078.mlab.com:47078/cxdemo
+export MONGODB_CREDENTIALS=mongodb://cxdemo:cxdemo@ds247078.mlab.com:47078/cxdemo
 
 # finally, run the app in dev mode...
 npm run dev
